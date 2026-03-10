a6f34b14db67045d1c3235b1d5a88f8ed1b9d9b0
TFJ_5183 <127980138+TFJ2021@users.noreply.github.com>
2026-03-10 17:43:44 +0100

    initial commit

---
+++ src/main/kotlin/net/tfj/tfjsScoreboardAPI/TfjsScoreboardAPI.kt
@@ -0,0 +1,14 @@
+package net.tfj.tfjsScoreboardAPI
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TfjsScoreboardAPI : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
